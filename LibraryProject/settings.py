65f3af35fd2546da682cb312f52b7bd2b587a21c
Peniel Negasa <penaye779@gmail.com>
2026-01-18 15:11:55 +0300

    Final ALX-ready folder structure with relationship_app

--- LibraryProject/settings.py
+++ LibraryProject/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'relationship_app',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
