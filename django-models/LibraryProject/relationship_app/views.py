86e62142b3c342940858c86a461efeadcab7ad1c
Peniel Negasa <penaye779@gmail.com>
2026-01-23 21:14:54 +0300

    Fix ALX imports and views structure

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Library, Book
 
-# Function-based view
-def book_list(request):
+
+def list_books(request):
     books = Book.objects.all()
-    return render(request, "relationship_app/list_books.html", {"books": books})
+    return render(
+        request,
+        "relationship_app/list_books.html",
+        {"books": books}
+    )
 
-# Class-based view
+
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
