529dd5e07d6142349ab7d8dbc02da513cb969efa
Peniel Negasa <penaye779@gmail.com>
2026-01-23 21:18:53 +0300

    Add ALX-compliant views, urls, and templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
+
 from .models import Library, Book
 
 
+# Function-based view
 def list_books(request):
-    books = Book.objects.all()
+    books = Book.objects.select_related("author").all()
     return render(
         request,
         "relationship_app/list_books.html",
-        {"books": books}
+        {"books": books},
     )
 
 
+# Class-based view
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
