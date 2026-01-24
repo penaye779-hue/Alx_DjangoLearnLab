c7a4beaf7e52ebe7e599ad687c615a0fe978ddde
Peniel Negasa <penaye779@gmail.com>
2026-01-24 11:24:49 +0300

    Fix Task 1 URLs and imports for ALX checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView 
-(
+from .views import (
     list_books, LibraryDetailView,
     register, CustomLoginView, CustomLogoutView,
     admin_view, librarian_view, member_view,
