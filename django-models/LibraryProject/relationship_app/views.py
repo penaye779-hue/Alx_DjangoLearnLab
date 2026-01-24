c0a44aa0afdf2fb1644273b252ca704069198708
Peniel Negasa <penaye779@gmail.com>
2026-01-24 12:07:16 +0300

    Add Tasks 2, 3, 4: authentication, role-based access, and permission-based book management

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import user_passes_test
 
 from .models import Book
 from .models import Library  # ALX requires this exact import
