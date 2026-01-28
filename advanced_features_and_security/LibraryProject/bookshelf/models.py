cbe85f1c095a54ed3609402b195c094fc057aea8
Peniel Negasa <penaye779@gmail.com>
2026-01-28 16:30:59 +0300

    Add custom user model and bookshelf app

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,8 +1,10 @@
+# LibraryProject/bookshelf/models.py
 from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
+    published_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.title
