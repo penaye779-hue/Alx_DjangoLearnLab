733ad6a0080e6f0426387a7e79dc62faf403c0ba
Peniel Negasa <penaye779@gmail.com>
2026-01-31 15:04:36 +0300

    Fix urls.py location and include api.urls for ALX checker

--- api_project/urls.py
+++ api_project/urls.py
@@ -1,3 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # must literally say 'api.urls'
+]
+
+
+
 """
 URL configuration for api_project project.
 
@@ -14,9 +24,3 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
