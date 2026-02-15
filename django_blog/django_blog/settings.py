6ec01176929ba14da9b1d8ce037c9c7d8d918aaf
Peniel Negasa <penaye779@gmail.com>
2026-02-16 00:02:35 +0300

    Fix task 0: database config keys USER and PORT as checker expects

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_blog_db",
+        "USER": "postgres",      # must be exactly 'USER'
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",          # must be exactly 'PORT'
     }
 }
 
