9904aa7244e3009406b2be9dcabc23e3cda72380
Peniel Negasa <penaye779@gmail.com>
2026-02-15 22:19:18 +0300

    Fix tagging and search functionality

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Comment
-from .models import Post
 from taggit.forms import TagWidget
-from django import forms
+
+from .models import Post, Comment
+
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -12,22 +12,18 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-from django import forms
-from .models import Post
 
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['title', 'content']
 
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['content']
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
         widgets = {
-            'tags': TagWidget(),  # Allows adding multiple tags
-        }        
+            'tags': TagWidget(),
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
