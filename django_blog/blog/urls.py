7bc1d42c815504c87cb60646371ca20c46aa6392
Peniel Negasa <penaye779@gmail.com>
2026-02-15 10:53:22 +0300

    Fix comment URLs and templates to match ALX checker

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('profile/', profile, name='profile'), 
 ]
 urlpatterns += [
-    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
